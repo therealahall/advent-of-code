f973ed3a120ae81df602790fb2049d4f4e9280a9
Aaron Hall <ahall@aaronhall.us>
2022-12-03 12:41:10 -0600

    feat(day-3): python solution for rucksacks

---
+++ 2022/day-3-rucksacks/phase1.py
@@ -0,0 +1,15 @@
+import string
+def convertLetterToPriority(letter):
+    return string.ascii_letters.index(letter) + 1
+
+file = open("input.txt", "r")
+list = file.read().replace("\n", ",").split(",")
+score = 0
+for line in list:
+    compartmentOne = line[:len(line)//2]
+    compartmentTwo = line[len(line)//2:]
+    commonalities = set(compartmentOne)&set(compartmentTwo)
+    for commonality in commonalities:
+        score += convertLetterToPriority(commonality)
+
+print(score)
---
+++ 2022/day-3-rucksacks/phase2.py
@@ -0,0 +1,21 @@
+import string
+def convertLetterToPriority(letter):
+    return string.ascii_letters.index(letter) + 1
+
+file = open("input.txt", "r")
+list = file.read().replace("\n", ",").split(",")
+score = 0
+groupings = []
+group = []
+
+for i, line in enumerate(list):
+    group.append(line)
+    if((i + 1) % 3 == 0):
+        groupings.append(group)
+        group = []
+
+for groups in groupings:
+    commonalities = set(groups[0])&set(groups[1])&set(groups[2])
+    for commonality in commonalities:
+            score += convertLetterToPriority(commonality)
+print(score)
